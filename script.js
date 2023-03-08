deb4e61965bebbc60f22af18deca6cf3a11d98f2
rexjoseph <rexjoseph96@gmail.com>
2023-03-08 03:57:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: 'How old is Rex?',
+    a: '10',
+    a: '17',
+    a: '26',
+    a: '110',
+    correct: 'c'
+  },
+  {
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'a'
+  },
+  {
+    question: 'Who is the President of US?',
+    a: 'Rex Joseph',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    correct: 'b'
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Helicopters Terminals Motorboats Lamborghinis',
+    correct: 'a'
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'd'
+  }
+]
